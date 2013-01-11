5b04061f7602570af200a728b4969cf45d0b5640
andydenenberg <andydenenberg@gmail.com>
2013-01-10 21:23:30 -0600

    mobile integration

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default :from => "andy@standtallcampaign.org"
+  default :from => "andy@denenberg.net"
   
   def welcome_email(user)
     mail(:to => user.email, :subject => "Welcome to our Colaboration!")
