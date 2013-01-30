f5be18cde1c14f0509611bcc5ff42b89360a0d08
andydenenberg <andydenenberg@gmail.com>
2013-01-30 09:33:22 -0600

    fixup plotting

--- lib/translog.rb
+++ lib/translog.rb
@@ -18,6 +18,23 @@ module  Translog
     PostLogger.debug(text)
   end
   
+  def self.parse_daily_snapshot
+    s = self.read_log
+    ds = s.collect { |l| l if l.include?('Daily Snapshot:') }.compact 
+    member = [ ]
+    ds.each do |line|
+      cut = line.split('Daily Snapshot:')
+      dt = cut[0].split(' ')
+      time = dt[1] + ' ' + dt[2].delete(']')
+      members = cut[1].split(',').collect { |mem| mem.split(' = ') }.collect { |m,n| [ m.delete(' '), n.to_f ] }.sort_by! { |a,b| a }
+      a = [ ]
+      members.each do |m|
+        a.push [ time, m[1], m[0] ]
+      end
+      member.push a
+    end
+    return member
+  end
   
 end
   
