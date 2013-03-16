60bdb591acec0df728c00f2a8c5c0557aef0fb33
andydenenberg <andydenenberg@gmail.com>
2013-03-15 20:30:34 -0500

    put in switch for heroku mailer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -21,7 +21,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "andy@denenberg.net"
+  config.mailer_sender = "andydenenberg@gmail.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
